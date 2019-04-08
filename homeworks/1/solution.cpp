3e928e32d3cc63f94d9075bc5b5d9c576bdb4bab
Honza Bittner <git@honzabittner.cz>
2019-04-08 21:40:25 +0200

    update

--- homeworks/1/solution.cpp
+++ homeworks/1/solution.cpp
@@ -31,7 +31,49 @@
 using namespace std;
 #endif /* __PROGTEST__ */
 
+class CombinedPriceList {
+public:
+    CombinedPriceList() = default;
+
+    set<AProducer> producers;
+    APriceList priceList;
+
+    void Add(const AProducer &producer, const APriceList &_priceList) {
+        // If producer haven't already added products to this priceList.
+        if (producers.find(producer) != producers.end()) {
+            for (auto &item : _priceList->m_List) {
+                // If there is a duplicity in a product (by width and height).
+                for (auto &addedItem : priceList->m_List) {
+                    if ((addedItem.m_H == item.m_H && addedItem.m_W == item.m_W) ||
+                        (addedItem.m_H == item.m_W && addedItem.m_W == item.m_H)) {
+                        // What is the lower cost?
+                        if (item.m_Cost < addedItem.m_Cost) {
+                            addedItem.m_Cost = item.m_Cost;
+                        }
+                    } // Different product.
+                    else {
+                        priceList->Add(item);
+                    }
+                }
+                priceList->Add(item);
+            }
+        }
+    }
+};
+
 class CWeldingCompany {
+private:
+    vector<thread> threads;
+    set<AProducer> producers;
+    set<ACustomer> customers;
+    map<unsigned int, CombinedPriceList> priceLists; // material: combined price list
+
+    // TODO: buffery?
+
+    void Process();
+
+    void ProcessCustomer();
+
 public:
     static void SeqSolve(APriceList priceList, COrder &order);
 
@@ -43,31 +85,96 @@ public:
 
     void Start(unsigned thrCount);
 
-    void Stop(void);
+    void Stop();
 };
 
 void CWeldingCompany::SeqSolve(APriceList priceList, COrder &order) {
+#ifdef MY_DEBUG
+    cout << "SeqSolver" << endl;
+#endif
 
+    vector<COrder> orderList;
+    orderList.emplace_back(order);
+    ProgtestSolver(orderList, priceList);
 }
 
 void CWeldingCompany::AddProducer(AProducer prod) {
+#ifdef MY_DEBUG
+    cout << "Add producer " << prod << endl;
+#endif
 
+    // TODO: maybe some mutex?
+    producers.insert(prod);
 }
 
 void CWeldingCompany::AddCustomer(ACustomer cust) {
+#ifdef MY_DEBUG
+    cout << "Add customer " << cust << endl;
+#endif
 
+    // TODO: maybe some mutex?
+    customers.insert(cust);
+    threads.emplace_back(&CWeldingCompany::ProcessCustomer, this);
 }
 
 void CWeldingCompany::AddPriceList(AProducer prod, APriceList priceList) {
+#ifdef MY_DEBUG
+    cout << "Add priceList " << priceList << " from producer " << prod << endl;
+#endif
 
+    // TODO: maybe some mutex?
+
+    if (priceLists.find(priceList->m_MaterialID) != priceLists.end()) {
+        CombinedPriceList cpl;
+        cpl.Add(prod, priceList);
+        priceLists.insert(make_pair(priceList->m_MaterialID, cpl));
+    } else {
+        priceLists[priceList->m_MaterialID].Add(prod, priceList);
+    }
 }
 
 void CWeldingCompany::Start(unsigned thrCount) {
+#ifdef MY_DEBUG
+    cout << "Starting with " << thrCount << " threads" << endl;
+#endif
 
+    // Create threads.
+    for (unsigned int i = 0; i < thrCount; i++) {
+        threads.emplace_back(&CWeldingCompany::Process, this);
+    }
 }
 
-void CWeldingCompany::Stop(void) {
+void CWeldingCompany::Stop() {
+#ifdef MY_DEBUG
+    cout << "Stopping" << endl;
+#endif
+    // TODO: počkat na obsloužení všech zákazníků a na naceněné a vrácené poptávky – to dát do Process
+    // TODO: notify všechny podmínky, aby se probraly
 
+    // Wait for threads to finish.
+    for (auto &thread : threads) {
+        thread.join();
+    }
+
+#ifdef MY_DEBUG
+    cout << "Stopped" << endl;
+#endif
+}
+
+void CWeldingCompany::Process() {
+#ifdef MY_DEBUG
+    cout << "Starting Process thread" << endl;
+#endif
+
+    // TODO
+}
+
+void CWeldingCompany::ProcessCustomer() {
+#ifdef MY_DEBUG
+    cout << "Starting ProcessCustomer thread" << endl;
+#endif
+
+    // TODO
 }
 
 // TODO: CWeldingCompany implementation goes here
@@ -75,7 +182,7 @@ void CWeldingCompany::Stop(void) {
 //-------------------------------------------------------------------------------------------------
 #ifndef __PROGTEST__
 
-int main(void) {
+int main() {
     using namespace std::placeholders;
     CWeldingCompany test;
 
